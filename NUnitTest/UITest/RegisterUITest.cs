fce3f7d753fe1f83b416b0901abd0aa42dc11899
Xing <x.xing.unsw@gmail.com>
2020-05-26 22:04:37 +1000

    Remove some unnecessary properties

--- NUnitTest/UITest/RegisterUITest.cs
+++ NUnitTest/UITest/RegisterUITest.cs
@@ -61,7 +61,6 @@ namespace UITesting
             public string Email { get; set; }
             public string Mobile { get; set; }
             public string FirstName { get; set; }
-            public int NumberOfGuest { get; set; }
             public string LastName { get; set; }
             public string Password { get; set; }
             public string PasswordConfirm { get; set; }
